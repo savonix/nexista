79be67651dc64a359835913281e42c098a7eb1a3
Albert Lash <albert.lash@docunext.com>
2007-10-27 03:49:41 +0000

    making it easier to shut off xsltcache

--- nexista/modules/handlers/xsl.handler.php
+++ nexista/modules/handlers/xsl.handler.php
@@ -39,34 +39,39 @@ class XslHandler
         
         $flow = Flow::Singleton();
         
-         // The following can be used with the NYT xslt cache.
+        // The following can be used with the NYT xslt cache.
+        // Uncomment to use xsltcache: 
+        // $use_xslt_cache = "yes";
         $tmpfile="/tmp/xsl/_tmp_".basename($xslfile);
         if(!is_file(tmpfile)) {        
-        
-        $xsl = new DomDocument;
-		$xsl->substituteEntities = true;
-        $xslfilecontents = 
+            
+            $xsl = new DomDocument;
+            $xsl->substituteEntities = true;
+            $xslfilecontents = 
         
 '<!DOCTYPE xslt [
 <!ENTITY nx_project_xsl "'.PROJECT_ROOT.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'xsl'.DIRECTORY_SEPARATOR.'">
 <!ENTITY nx_app_xsl "'.NX_PATH_APPS.'templates'.DIRECTORY_SEPARATOR.'xsl'.DIRECTORY_SEPARATOR.'"> ]>';
 
-        if(!is_file($xslfile)) { 
-            Error::init('XSL Handler - Error processing XSL file - it is unavailable: '.$xslfile, NX_ERROR_FATAL);
+            if(!is_file($xslfile)) { 
+                Error::init('XSL Handler - Error processing XSL file - it is unavailable: '.$xslfile, NX_ERROR_FATAL);
+            }
+            $xslfilecontents .= file_get_contents($xslfile);
+            
+            $xsl->loadXML($xslfilecontents); 
+            
+            $xslHandler = new XsltProcessor;
+            $xslHandler->importStyleSheet($xsl); 
+            
+            if($use_xslt_cache=="yes") { 
+                $xsl->save($tmpfile);
+            }
         }
-        $xslfilecontents .= file_get_contents($xslfile);
-        
-        $xsl->loadXML($xslfilecontents); 
-        
-        $xslHandler = new XsltProcessor;
-        $xslHandler->importStyleSheet($xsl); 
-        
-
-        $xsl->save($tmpfile);
+        if($use_xslt_cache=="yes") { 
+            $xslHandler = new xsltCache;
+            $xslHandler->importStyleSheet($tmpfile); 
         }
-        $xslHandler = new xsltCache;
-        $xslHandler->importStyleSheet($tmpfile); 
-      
+        
         $output = $xslHandler->transformToXML($flow->flowDocument); 
       
         if($output === false)
