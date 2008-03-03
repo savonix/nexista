b54a03b4fb48b0b4cac099e1aa457a5553ad762a
Albert Lash <albert.lash@docunext.com>
2008-03-03 17:39:23 +0000

    updated package and conducting a major overhaul of nx cache output buffer

--- plugins/nx_cache.php
+++ plugins/nx_cache.php
@@ -7,77 +7,41 @@ Version: 0.1
 Copyright: Savonix Corporation
 Author: Albert Lash
 License: LGPL
+Status: beta
 */
 
-
-$cache_config = (bool)Nexista_Config::get("./plugins/nx_cache/active");
-if($cache_config===true){
-    if(count($_POST) > 0 || $_GET['from_date'] || $_GET['nid']=="logout") { 
-        $gate_cache_file = NX_PATH_CACHE.'cache_*';
-        
-        foreach (glob("$gate_cache_file") as $filename) {
-           unlink($filename);
-        }
-        
-    }
-}
-
-if($cache_config===false) {
-} else { 
-Nexista_Init::registerOutputHandler('gzBuffer');
-}
-
-function gzBuffer($init)
-{
-	$init->process();
-	
-	ob_start();
-    if(!(bool)Nexista_Config::get('./plugins/nx_cache/compress')) { 
-        ob_start();
-    } else { 
-        ob_start('ob_gzhandler');
-    }
-    $cache_config = (bool)Nexista_Config::get('./plugins/nx_cache/active');
-    if($cache_config===false) { 
-        echo $init->run();
-        ob_end_flush();
-        header("Content-Length: ".ob_get_length());
-        ob_end_flush();
-        exit;
-    } else {
+if(count($_POST) > 0 || $_GET['from_date'] || $_GET['nid']=="logout") { 
+    $gate_cache_file = NX_PATH_CACHE.'cache_*';
     
-	$request_uri = $_SERVER['REQUEST_URI'];
-    Nexista_Flow::add("request_uri",$request_uri);
-	$expiryTime=$init->getInfo('cacheExpiryTime');
-	$my_request_uri = $request_uri;
-	$clear_gate_file='cache_'.$my_user_id."_".$my_request_uri;
-	if(!include('Cache/Lite.php')) { 
-        echo "Error: Can't find PEAR package cache_lite";
-        exit;   
+    foreach (glob("$gate_cache_file") as $filename) {
+       unlink($filename);
     }
+}
+
+Nexista_Init::registerOutputHandler('nx_cache');
+
+function nx_cache($init)
+{
+
     if(!is_dir(NX_PATH_CACHE)) { 
         mkdir(NX_PATH_CACHE);
     }
-        
+
+	$init->process();
+
+	ob_start();
+	$my_request_uri = $_SERVER['REQUEST_URI'];
+	$expiryTime=$init->getInfo('cacheExpiryTime');
+	$clear_gate_file='cache_'.$my_user_id."_".$my_request_uri;
 
 	$options = array('cacheDir'=> NX_PATH_CACHE,'caching'  => $cache_config,'lifeTime' => $expiryTime);
 	$cache = new Cache_Lite($options);
 
-
-	if(isset($_SESSION['NX_AUTH']['real_account_id']) && $file_server_status!="yes") { 
-		cs_console();
-	}
-    
-    
 	// Server cache! Always on, controlled by sitemap.
-	// Server cache especially helpful for ssl connections.
 	if($output = $cache->get($my_request_uri, $my_user_id, TRUE)) { 
-		$cache_type = "File";
-		// Check file mtime, and potentially return 304 response code.
-		// Only for testing and production sites.
-		$mynid = NX_PATH_CACHE.'cache_'.md5($my_user_id).'_'.md5($my_request_uri); 		
+		ob_start();
+        $mynid = NX_PATH_CACHE.'cache_'.md5($my_user_id).'_'.md5($my_request_uri); 		
 		$last_modified_str = filemtime($mynid);
-		$file_size = filesize($mynid);
 		$etag = md5($last_modified_str);
 		$last_modified = gmdate('D, d M Y H:i:s', $last_modified_str);
 		
@@ -86,29 +50,15 @@ function gzBuffer($init)
         // to use the cache without checking the server to ask if anything 
         // has been modified
         // Should we set it here?
-        if(function_exists(getallheaders)) {  
-            $request = getallheaders(); // Apache Module Only
-            if (isset($request['If-Modified-Since'])) { 
-               $modifiedSince = explode(';', $request['If-Modified-Since']); 
-               $modifiedSince = strtotime($modifiedSince[0]); 
-            } else { 
-               $modifiedSince = 0; 
-            }
+        if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) { 
+           $modifiedSince = $_SERVER['HTTP_IF_MODIFIED_SINCE']; 
+           $modifiedSince = strtotime($modifiedSince); 
         } else { 
-            // CGI
-            if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) { 
-               $modifiedSince = $_SERVER['HTTP_IF_MODIFIED_SINCE']; 
-               $modifiedSince = strtotime($modifiedSince); 
-            } else { 
-                $modifiedSince = 0;
-            }
+            $modifiedSince = 0;
         }
         
 		// Client cache!
         $client_cache = $init->getInfo('clientCacheExpiryTime');
-        // Should we calculate the time? Sure... need to make sure the 
-        // modified time plus the client cache is greater than the 
-        // if modified since time...
         $lms = $modifiedSince;
         $client_cache_work = 
             mktime(date('H',$lms), date('i',$lms), date('s',$lms)+$client_cache, 
@@ -117,61 +67,35 @@ function gzBuffer($init)
         if($client_cache > 0 && $client_cache_work > time()) {
             while (@ob_end_clean());
             header( 'Cache-Control: no-cache, must-revalidate, post-check='.$client_cache.', pre-check='.$client_cache);
-            header("HTTP/1.0 304 Not Modified");
+            header("HTTP/1.1 304 Not Modified");
             exit();
         }
         
         // When using client cache a session cache limiter, you've got to use this cache-control
         // header.
-        header( 'Cache-Control: no-cache, must-revalidate, post-check=3600, pre-check=3600');
+        header("Cache-Control: no-cache, must-revalidate, post-check=3600, pre-check=3600");
 		header("Last-Modified: " . $last_modified . " GMT");
 	
 	} else { 
-        header( 'Cache-Control: no-cache, must-revalidate, post-check=3600, pre-check=3600');
+        if(!(bool)Nexista_Config::get('./plugins/nx_cache/compress')) { 
+            ob_start();
+        } else { 
+            ob_start('ob_gzhandler');
+        }
+
+        header("Cache-Control: no-cache, must-revalidate, post-check=3600, pre-check=3600");
 		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 		$output = $init->run();
-		/* Write the output to cache, only if its not encrypted data. */
-		$encrypt = Nexista_Path::get("//get_wiki_page/encrypt", "flow"); 
-		if($expiryTime > 2 && $encrypt!="1" && $_GET['view_flow']!=true) { 
-			$cache->save($output, $my_request_uri, $my_user_id);
-		}
+        $cache->save($output, $my_request_uri, $my_user_id);
 		$cache_type = "None";
 	}
 	if(isset($etag)) { 
         header("ETag: ".$etag);
     }
 	echo $output;
-    
 	ob_end_flush();
-	
-	
 	header("Content-Length: ".ob_get_length());
 	ob_end_flush();
 	
-	
-	}
-	
-	
+
 }
- 
-
-
-
-
-
-
-
-
-/* This function used by all stages. */
-function cs_console()  {
-	$blah = new XsltProcessor();
-	$xsl = new DomDocument;
-	$xsl->load(NX_PATH_APPS."_shared/xsl/impersonate_header.xsl");
-	$blah->importStyleSheet($xsl);
-	$flow = Flow::singleton();
-	$user_name=$_SESSION['NX_AUTH']['user_name'];
-	Nexista_Flow::add("user_name",$user_name);
-	echo $blah->transformToXML($flow->flowDocument);
-}
-
-
