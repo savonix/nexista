ab357f1d688542170cedcea72d62b3f303c05ea4
Albert Lash <albert.lash@docunext.com>
2008-04-27 18:24:54 +0000

    not sure if its used, but moved it to private wiki

--- modules/actions/upload.action.php
+++ modules/actions/upload.action.php
@@ -44,38 +44,36 @@ class Nexista_uploadAction extends Nexista_Action
 
     protected  function main()
     {
-    
+
         //see if a path is given. we default to Nexista temp dir
         if(!$dest = Nexista_Path::get($this->params['dest'], 'flow')) {
 			$dest = empty($this->params['dest']) ? NX_PATH_TMP : trim($this->params['dest'],'/').'/';
 		}
 
-		
         if(!empty($_FILES[$this->params['file']]['tmp_name']))
         {
-		
+
 		$name = $_FILES[$this->params['file']]['name'];
 	    $prefix = Nexista_Path::get($this->params['prefix'],"flow");
-	    if($prefix!='') {	 	
+	    if($prefix!='') {
 		    $unique_id = $prefix;
-		    $name=$unique_id."_".$name;    
+		    $name=$unique_id."_".$name;
 	    }
             if(!move_uploaded_file($_FILES[$this->params['file']]['tmp_name'], $dest.$name))
             {
-                Error::init('Upload action was unable to move uploaded file: '.$name . '. Check '.$dest.' permissions', NX_ERROR_WARNING);
+                Nexista_Error::init('Upload action was unable to move uploaded file: '.$name . '. Check '.$dest.' permissions', NX_ERROR_WARNING);
                 return false;
             }
-    
+
             //assign full destination path to flow as it can be useful
             $res = Nexista_Flow::find('//_files/file');
             if($res->length === 1)
             {
-				
+
                 Nexista_Flow::add('new_dir', $dest,$res->item(0));
-                //Flow::add('new_name', $name,$res->item(0));
                 Nexista_Flow::add('new_name', $dest.$name,$res->item(0));
-            
-    
+
+
             }
             chmod($dest.$name,0644);
         }
