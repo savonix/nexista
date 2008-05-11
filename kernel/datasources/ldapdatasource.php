ba39552811eb20bf2d24be123456952f898548a4
Albert Lash <albert.lash@docunext.com>
2008-05-11 14:20:58 +0000

    cleanup up ldap to prep for more development

--- kernel/datasources/ldapdatasource.php
+++ kernel/datasources/ldapdatasource.php
@@ -13,7 +13,7 @@
  * @subpackage  Datasources
  * @author      Albert Lash
  */
- 
+
 /**
  * This class provides functionality to access
  * ldap directories via PEAR's Net_LDAP package.
@@ -132,10 +132,10 @@ class Nexista_ldapDatasource
 
     public function setConnection()
     {
-    
+
         // Inclusion of the Net_LDAP package:
         require_once 'Net/LDAP.php';
-    
+
         // The configuration array:
         $config = array (
             'binddn'        => $this->params['binddn'],
@@ -143,44 +143,20 @@ class Nexista_ldapDatasource
             'basedn'        => $this->params['basedn'],
             'host'          => $this->params['hostname']
         );
-        
+
         // Connecting using the configuration:
         $this->ldap = Net_LDAP::connect($config);
-        
+
         // Testing for connection error
         if (PEAR::isError($ldap)) {
             die('Could not connect to LDAP-server: '.$ldap->getMessage());
         }
     }
 
-    public function prepareQuery() 
-    {
-        // What type of transaction?
-        switch($this->queryType)
-        {
-            case "search":
-                break;
-            case "add":
-                break;
-            default:
-                break;
-        }
-        
-        // Depending on transaction type, what are the parameters?
-        
-        // Execute the parameters
-        
-    }
-    
-    public function getEntry()
-    {
-        
-        
-    }
-    
+
     public function execQuery($search,$queryName,$filter,$options='')
     {
-        
+
         $this->queryName =& $queryName;
         // Test for search errors:
         if (PEAR::isError($this->ldap)) {
@@ -197,22 +173,7 @@ class Nexista_ldapDatasource
             die($search->getMessage() . "\n");
         }
         $this->storeResult();
-        
-    }
-    
-    public function moveEntry()
-    {
-        
-    }
-    
-    public function deleteEntry()
-    {
-        
-    }
-    
-    public function addEntry()
-    {
-        
+
     }
 
     /**
@@ -234,42 +195,8 @@ class Nexista_ldapDatasource
             $q->appendChild($flow->flowDocument->createElement($key,$myval));
             //echo "$ip    $cn\n";
         }
-
-        /* 
-        // from mdb2datasource
-		$debug = false;
-        
-        if($this->result_set)
-        {
-            $result_set = $this->result_set;
-
-            $cols = array_flip(array_keys($result_set[0]));
-			$row = 0;
-			$number_of_rows=count($result_set);
-            while($row < $number_of_rows)
-            {
-                $flow = Nexista_Flow::singleton();
-                $q = $flow->root->appendChild($flow->flowDocument->createElement($this->queryName));
-
-                foreach($cols as $key => $val)
-                {
-                    $myval = $result_set[$row][$key];
-                    if($debug===true) {
-                    echo "Stuff: $key, $myval<br/>";
-                    }
-                    $myval = htmlspecialchars($myval);
-                    $q->appendChild($flow->flowDocument->createElement($key,$myval));
-                }
-				$row++;
-                $xml_string .=  $flow->flowDocument->saveXml($q);
-            }
-            return true;
-        }
-        */
         return false;
     }
-
-}
-
+} // end class
 
 ?>
