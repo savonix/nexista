627ef60c6fb449a19a21b88648997a3afa2374b1
Albert Lash <albert.lash@docunext.com>
2008-05-25 00:15:46 +0000

    want the plugin not the action

---
+++ plugins/nx_rewrite.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * -File        rewrite.action.php
+ * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
+ * -Copyright   Nexista
+ * -Author      Albert Lash
+ */
+
+/**
+ * @package     Nexista
+ * @subpackage  Actions
+ * @author      Albert Lash
+ */
+ 
+/**
+ * This method attempts to recreate a subset of mod_rewrite's greatness.
+ * It takes the request URI, and then transforms it into a string of key 
+ * value pairs.
+ * 
+ * @package     Nexista
+ * @subpackage  Actions
+ */
+
+Nexista_Flow::registerImportHandler('nx_rewrite');
+
+function nx_rewrite($superglobals)
+{
+    $query_string = array(
+        'nid' => 'configurations',
+        'package' => 'postfix',
+        'page' => 'package'
+        );
+    if($superglobals==$_GET) {
+        $gate_key = Nexista_Config::get('./build/query');
+        if($superglobals[$gate_key]=="index") { 
+            return $query_string;
+        } else { 
+            return $superglobals;
+        }
+    } else {
+        return $superglobals;
+    }
+}
+?>
