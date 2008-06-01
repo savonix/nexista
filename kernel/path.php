1f6ff18fe68cba447b5fab0eaa4e0e1103870de5
Albert Lash <albert.lash@docunext.com>
2008-06-01 22:25:50 +0000

    path passes

--- kernel/path.php
+++ kernel/path.php
@@ -1,24 +1,32 @@
 <?php
-/*
+/**
  * -File        path.php
  * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
  * -Copyright   Nexista
  * -Author      Albert Lash
  * -Author      joshua savage
+ *
+ * PHP version 5
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
  */
 
 /**
- * @package Nexista
- * @author Joshua Savage
- */
-
-/**
- * This class provides 
+ * This class provides:
  *
  * -<b>string</b> - Plain string
  *
- *
- * @package     Nexista
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
  */
 
 class Nexista_Path
@@ -28,27 +36,28 @@ class Nexista_Path
      * Hold an instance of the class
      */
 
-    static private $instance;
+    static private $_instance;
 
     /**
      * Returns a string based on given protocol://path 
      *
-     * @param   string      path to resolve/render
-     * @param   string      (optional) default protocol if none given
-     * @return  string      value of variable
+     * @param string $path     path to resolve/render
+     * @param string $protocol (optional) default protocol if none given
+     *
+     * @return string value of variable
      */
     static public function get($path, $protocol = 'string')
     {
         switch($protocol) {
-            case 'flow':
-                $result = Nexista_Flow::getByPath($path);
-                break;
+        case 'flow':
+            $result = Nexista_Flow::getByPath($path);
+            break;
 
-            default:
-                $result = Nexista_Path::parseInlineFlow($path);
+        default:
+            $result = Nexista_Path::parseInlineFlow($path);
         }
-        if(!is_null($result))
-        {
+
+        if (!is_null($result)) {
             return $result;
         } else {
             return false;
@@ -61,31 +70,33 @@ class Nexista_Path
      * This method will look for curly bracketed values in a string
      * and return a flow expression.
      *
-     * @param       string      path to analyze, returns by ref
-     * @return      string      path with resolved inline flow expressions
+     * @param string $string path to analyze, returns by ref
+     * 
+     * @return string path with resolved inline flow expressions
      */
 
     static public function parseInlineFlow($string)
     {
         //replace bracketed flow expressions
-        $string = preg_replace_callback('~{(.*)}~U',create_function('$matches', 'return Nexista_Flow::getByPath($matches[1]);'),$string);
+        $string = preg_replace_callback('~{(.*)}~U', create_function('$matches', 
+            'return Nexista_Flow::getByPath($matches[1]);'), $string);
         return $string;
     }
 
 
     /**
-     * Returns a class Nexista_singleton.
+     * Returns a class singleton.
      *
-     * @return  object class Nexista_singleton instance
+     * @return object class singleton instance
      */
 
     static public function singleton()
     {
-        if (!isset(self::$instance)) {
+        if (!isset(self::$_instance)) {
             $c = __CLASS__;
-            self::$instance = new $c;
+            self::$_instance = new $c;
         }
-        return self::$instance;
+        return self::$_instance;
     }
 }
 
