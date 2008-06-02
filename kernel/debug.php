342d9b4d359435c4f02653876174fa6cccfdbe95
Albert Lash <albert.lash@docunext.com>
2008-06-02 01:57:16 +0000

    debug is in the house

--- kernel/debug.php
+++ kernel/debug.php
@@ -1,21 +1,29 @@
 <?php
-/*
- * -File        debug.php
- * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
- * -Copyright   Nexista
- * -Author      joshua savage
- * -Author      Albert Lash
- */
-
 /**
- * @package Nexista
- * @author Joshua Savage
- * @author Albert Lash
+ * -File        Debug.php
+ * -Copyright   Nexista
+ * -Author      Joshua Savage
+ * -Author      Albert Lash
+ *
+ * PHP version 5
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
  */
 
 /**
  * This class Nexista_provides some debugging functions
- * @package     Nexista
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
  */
 
 class Nexista_Debug
@@ -24,31 +32,35 @@ class Nexista_Debug
     /**
      * Pretty prints an array
      *
-     * @param    array       The array to dump to screen
-     * @param    name        Optional pretty name to print
+     * @param array $array The array to dump to screen
+     * @param name  $name  Optional pretty name to print
+     *
+     * @return null
      */
 
     static public function dump($array, $name = '')
     {
-        echo '<font color="orange"><br>--- <b>Begin array dump:</b> ' . $name . '---<br></font><pre>';
-        if (!empty($array))
-        {
+        echo '<font color="orange"><br>--- <b>Begin array dump:</b> ' . $name .
+            '---<br></font><pre>';
+
+        if (!empty($array)) {
             echo '<font color="purple">';
             print_r($array);
             echo '</font>';
-        }
-        else
-        {
+        } else {
             echo "Empty array:";
         }
-        echo '</pre><br><font color="orange"><b>--- End array dump ---</b></font>';
+        echo '</pre><br>
+        <font color="orange"><b>--- End array dump ---</b></font>';
     }
 
 
     /**
      * Prints out the arguments from a function
      *
-     * @param   string  argument array from a function
+     * @param string &$args argument array from a function
+     *
+     * @return null
      */
 
     static public function args(&$args)
@@ -56,21 +68,16 @@ class Nexista_Debug
 
         echo "<br><b>Function Arguments:</b><br>";
 
-        foreach($args as $key=>$val)
-        {
+        foreach ($args as $key=>$val) {
             $index = $key + 1;
 
-            if (is_array($val) && $this->array_mode)
-            {
+            if (is_array($val) && $this->array_mode) {
                 echo "Arg #$index: ";
                 $this->array_dump($val);
                 echo "<br>";
-            }
-            else
-            {
+            } else {
                 echo "Arg #$index: $val<br>";
             }
-
         }
         echo "<br>";
     }
@@ -84,46 +91,51 @@ class Nexista_Debug
      * 'in' - sets function entry
      * 'out' - sets function exit - prints elapsed time
      *
-     * @param   string      type of registration
-     * @param   string      module name
-     * @return  string      elapsed time
+     * @param string $type     type of registration
+     * @param string $function module name
+     *
+     * @return string elapsed time
      */
 
     static public function register($type, $function)
     {
 
-        switch($type)
-        {
-            case 'in':
+        switch($type) {
+        case 'in':
+            $pos = 0;
+            if (isset($GLOBALS['debugTrackModule'])) {
+                $pos = @count($GLOBALS['debugTrackModule']);
+            }
 
-                $pos = 0;
-                if (isset($GLOBALS['debugTrackModule']))
-                {
-                    $pos = @count($GLOBALS['debugTrackModule']);
-                }
+            $GLOBALS['debugTrackModule'][$pos]['name']      = $function;
+            $GLOBALS['debugTrackModule'][$pos]['startTime'] = microtime();
 
-                $GLOBALS['debugTrackModule'][$pos]['name'] = $function;
-                $GLOBALS['debugTrackModule'][$pos]['startTime'] = microtime();
+            $indent = ($pos) * 6 ;
+            Nexista_Debug::message(str_pad('> ', $indent, '-', STR_PAD_LEFT).
+                '<b>'.$function .'</b>');
 
-                $indent = ($pos) * 6 ;
-                Nexista_Debug::message(str_pad('> ', $indent,'-', STR_PAD_LEFT). '<b>'.$function .'</b>');
+            break;
 
-                break;
+        case 'out':
+            $pos = @count($GLOBALS['debugTrackModule']) - 1;
 
-            case 'out':
-                $pos = @count($GLOBALS['debugTrackModule']) - 1;
-                $indent = $pos * 6 ;
+            $indent = $pos * 6 ;
 
+            $start_time = $GLOBALS['debugTrackModule'][$pos]['startTime'];
 
-                $GLOBALS['debugTrackModule'][$pos]['elapsedTime'] = Nexista_Debug::profile($GLOBALS['debugTrackModule'][$pos]['startTime']);
-                Nexista_Debug::message(str_pad('< ', $indent, "-", STR_PAD_LEFT). '<b>'.$function . ' @ </b>' . $GLOBALS['debugTrackModule'][$pos]['elapsedTime'].' seconds');
+            $GLOBALS['debugTrackModule'][$pos]['elapsedTime'] =
+                Nexista_Debug::profile($start_time);
 
-                unset($GLOBALS['debugTrackModule'][$pos]);
-                break;
+            Nexista_Debug::message(str_pad('< ', $indent, "-", STR_PAD_LEFT).
+                '<b>'.$function . ' @ </b>'.
+                $GLOBALS['debugTrackModule'][$pos]['elapsedTime'].' seconds');
+
+            unset($GLOBALS['debugTrackModule'][$pos]);
+            break;
 
         }
 
-        // This is for debugging the debug class. Probably don't want to ever use this now. 
+        // This is for debugging the debug class. Probably don't need this now
         //Nexista_Debug::dump($GLOBALS['debugTrackModule']);
 
     }
@@ -135,15 +147,16 @@ class Nexista_Debug
      * This function accepts a message that can be used to debug
      * or trace script flow. Depending on the setting of
      *
-     * @param   string      message to log/print
-     * @return  string      elapsed time
+     * @param string $message message to log/print
+     *
+     * @return string elapsed time
      */
 
     static public function message($message)
     {
-        if (isset($GLOBALS['debugTrack']) && $GLOBALS['debugTrack'] === true)
-        {
-            echo '<br><span style="color: purple;" class="debug">' . $message.'</span>';
+        if (isset($GLOBALS['debugTrack']) && $GLOBALS['debugTrack'] === true) {
+            echo '<br><span style="color: purple;" class="debug">'.
+                $message.'</span>';
         }
     }
 
@@ -151,42 +164,43 @@ class Nexista_Debug
     /**
      * Returns current time in seconds
      *
-     * @param   string      microtime
-     * @return  string      elapsed time
+     * @param string $start microtime
+     *
+     * @return string elapsed time
      */
 
     static public function getMicrotime($start)
     {
 
-        list($usec, $sec) = explode(" ",microtime());
+        list($usec, $sec) = explode(" ", microtime());
         return ((float)$usec + (float)$sec);
     }
 
     /**
      * Calculates elapsed time
      *
-     * @param   integer     start time to use. If none supplied, script start time is used
-     * @return  string      elapsed time
+     * @param integer $startTime start time to use. If none, script start time
+     *
+     * @return string elapsed time
      */
 
     static public function profile($startTime = false)
     {
-        list($usec, $sec) = explode(" ",microtime());
+        list($usec, $sec) = explode(" ", microtime());
+
         $end = ((float)$usec + (float)$sec);
 
-        if (!$startTime)
-        {
+        if (!$startTime) {
             $start = $GLOBALS['debugStartTime'];
-        }
-        else
-        {
+        } else {
             $start = &$startTime;
         }
 
         list($usec, $sec) = explode(" ", $start);
+
         $start = ((float)$usec + (float)$sec);
 
-        return number_format($end - $start,3);
+        return number_format($end - $start, 3);
 
     }
 
