58f3757309d4953350dcefe025f326e13b44436b
Albert Lash <albert.lash@docunext.com>
2008-06-13 21:25:59 +0000

    improving nexista cache - working pretty well now

--- extensions/nexista_cache.php
+++ extensions/nexista_cache.php
@@ -28,34 +28,106 @@ Configuration:
     <placement>prepend</placement>
     <source>&includepath;extensions/nexista_cache.php</source>
     <active>1</active>
+    <!-- refresh divisor:
+    a value of 10 would touch a still fresh cache with less than
+    1/10th of the cache lifetime, making if fresh again.
+    since it has a new modification time, it will be refreshed by the
+    client, but it won't be invalidated.
+    -->
+    <refresh_divisor></refresh_divisor>
     <timers>1</timers>
     <excludes></excludes>
     <purge_gates>list-of-gates,logout</purge_gates>
     <purge_gets>from_date</purge_gets>
+    <gc_divisor></gc_divisor>
+    <absmaxlife></absmaxlife>
 </nexista_cache>
 */
 
-/* PURGE CACHE */
-if ( // POST will always purge the cache
-    count($_POST) > 0 ||
-    // need a foreach loop here
-    $_GET['from_date'] ||
-    // and here
-    $_GET['nid']=="logout" ) {
-    $gate_cache_file = NX_PATH_CACHE.'cache_*';
 
-    foreach (glob("$gate_cache_file") as $filename) {
-        unlink($filename);
+$active = (bool)Nexista_Config::get("./extensions/nexista_cache/active");
+
+if($active) {
+    /* PURGE CACHE */
+    if ( // POST will always purge the cache
+        count($_POST) > 0 ||
+        // need a foreach loop here
+        $_GET['from_date'] ||
+        // and here
+        $_GET['nid']=="logout" ) {
+        $gate_cache_file = NX_PATH_CACHE.'cache_*';
+
+        foreach (glob("$gate_cache_file") as $cac) {
+            unlink($cac);
+        }
+
+    } else {
+        /* Garbage Collection */
+        /* TODO - Finish this */
+        if (rand(0,100) < 10) {
+            // Do GC
+            $gate_cache_file = NX_PATH_CACHE.'cache_*';
+
+            foreach (glob("$gate_cache_file") as $cac) {
+                $lm  = filemtime($cac);
+
+                if (!$absmaxlife) 
+                    $absmaxlife = 3600;
+
+                if (time() > $lm + $absmaxlife) {
+                    unlink($cac);
+                }
+            }
+        }
+        // Note - Cache_Lite has automatic cleaning - does it work?
+        // Sounds good:
+        /*
+        * Disable / Tune the automatic cleaning process
+        *
+        * The automatic cleaning process destroy too old (for the given life time)
+        * cache files when a new cache file is written.
+        * 0               => no automatic cache cleaning
+        * 1               => systematic cache cleaning
+        * x (integer) > 1 => automatic cleaning randomly 1 times on x cache write
+        *
+        */
     }
 }
 
-
 /* Handle Exclusions */
+$included = true;
+$excludes = Nexista_Config::get('./extensions/nexista_cache/excludes');
+if(strpos($excludes,',')) {
+    $x_array = explode(',',$excludes);
+} else {
+    if(!empty($excludes)) {
+        $x_array[] = $excludes;
+    }
+}
+
+if(!empty($x_array)) {
+    if(in_array($_GET['nid'],$x_array)) {
+        unset($included);
+    } else {
+        // this could be slow, might want to have a setting to turn on / off
+        foreach($x_array as $value) {
+            if(eregi($value,$_GET['nid'])) {
+                unset($included);
+            }
+        }
+    }
+}
+/* END EXCLUSIONS */
+
+
+if ($included && $active) {
+    Nexista_Init::registerOutputHandler('Nexista_cache');
+}
 
 
 
 
-Nexista_Init::registerOutputHandler('Nexista_cache');
+
 
 /**
  * nexista_cache
@@ -70,54 +142,59 @@ function Nexista_cache($init)
 {
 
     //configuration - move to xml
-    $timers = false;
-    $active = true;
+    $timers = true;
 
     //necessary stuff no matter what
+    $init->process();
     $uid = Nexista_Flow::get('//runtime/user_id');
     $uri = $_SERVER['REQUEST_URI'];
     $cac = NX_PATH_CACHE . 'cache_' . md5($uid) . '_' . md5($uri);
+    $exp = $init->getInfo('cacheExpiryTime');
+
 
     //first things first - check for if-modified-since
     if ($ims = $_SERVER['HTTP_IF_MODIFIED_SINCE']) {
         if (is_file($cac)) {
-            $ims = strtotime($ims);
-            $lm  = filemtime($cac);
-            if ($lm == $ims) {
-                header("HTTP/1.1 304 Not Modified");
-                exit;
+            $ims  = strtotime($ims);
+            $lm   = filemtime($cac);
+            $etag = md5(file_get_contents($cac));
+            $ctag = $_SERVER['HTTP_IF_NONE_MATCH'];
+            if ($lm <= $ims && $etag == $ctag) {
+                // Hasn't been modified, is it still fresh?
+                $fresh = $lm + $exp - time();
+                if( $fresh > 0 ) {
+                    header("HTTP/1.1 304 Not Modified");
+                    // If its close to expiring, extend life
+                    if( $fresh < $exp / 10 ) {
+                        touch($cac);
+                    }
+                    exit;
+                }
+                // if not fresh, it will be rebuilt
             }
         }
     }
 
 
-    $init->process();
-    $content_type = $init->getInfo('content_type');
-    $cache_control = $init->getInfo('cache_control');
-
-    if (!is_dir(NX_PATH_CACHE))
-        @mkdir(NX_PATH_CACHE, 0777, true);
-
     include_once 'Cache/Lite.php';
-
-    ob_start();
-    ob_start();
-
-
-    // Server cache
-    $expiryTime = $init->getInfo('cacheExpiryTime');
-
-    $active = Nexista_Config::get("./extensions/nexista_cache/active");
-
+    /* TODO - Add autocleaning */
     $options = array(
             'cacheDir' => NX_PATH_CACHE,
-            'caching'  => $active,
-            'lifeTime' => $expiryTime
+            'caching'  => true,
+            'automaticCleaningFactor' => 0,
+            'readControl'  => false, // necessary to send etag
+            'lifeTime' => $exp
             );
 
     $cache = new Cache_Lite($options);
 
-    if ($output = $cache->get($uri, $uid, true)) {
+    $content_type = $init->getInfo('content_type');
+    $cache_control = $init->getInfo('cache_control');
+
+    ob_start();
+    ob_start();
+
+    if ($output = $cache->get($uri, $uid)) {
         $cache_type = "file cache";
         $lm = filemtime($cac);
         header("Last-Modified: " . gmdate('D, d M Y H:i:s', $lm) . " GMT");
@@ -136,13 +213,18 @@ function Nexista_cache($init)
                 $output = str_replace("</body>", "", $output);
                 $output = str_replace("</html>", "", $output);
 
-                $output .= "\n\n<!--\nOriginal request required: $server_time!\n-->\n\n";
+                $output .= "\n\n<!--\nOriginal request required: $server_time! \n-->\n\n";
                 $output .= "</body></html>";
             } elseif ( $content_type == "text/css" ) {
                 $output .=  "\n/* Original request required: $server_time! */";
             }
         }
+
+        if (!is_dir(NX_PATH_CACHE))
+            @mkdir(NX_PATH_CACHE, 0700, true);
+
         $cache->save($output, $uri, $uid);
+        header("ETag: ".md5($output));
     }
 
     if ( $content_type == "text/html" ) {
@@ -160,14 +242,15 @@ function Nexista_cache($init)
             echo "Output generated by $cache_type in $server_time.\n";
             echo "Output sent with Cache-Control: $cache_control headers, which will affect future requests.\n";
             if ($lm > 0) {
+                echo " Last modified:  $lm. ";
                 echo "Cache created: ". gmdate('D, d M Y H:i:s', $lm) . " GMT\n";
             }
             echo "Current time: " . gmdate('D, d M Y H:i:s') . " GMT\n-->\n";
             echo "</body></html>";
         } elseif ( $content_type == "text/css" ) {
-            echo "\n/* Output generated by $cache_type in $server_time. */";
+            echo "\n/* Output generated by $cache_type in $server_time. Last modified:  $lm. */";
         } elseif ( $content_type == "text/x-javascript" ) {
-            echo "\n// Output generated by $cache_type in $server_time.";
+            echo "\n// Output generated by $cache_type in $server_time. Last modified:  $lm. ";
         }
     }
     ob_end_flush();
