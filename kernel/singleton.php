21c69440d50a164222b0bf88a03403e0f3ce3e64
Albert Lash <albert.lash@docunext.com>
2008-06-26 03:22:49 +0000

    using a singleton registry instead of a singleton function in every class

---
+++ kernel/singleton.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * -File        Session.php
+ * -License     LGPL (http://www.gnu.org/copyleft/lesser.html)
+ * -Copyright   Nexista
+ * -Author      Joshua Savage
+ * -Author      Albert Lash
+ *
+ * PHP version 5
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
+ */
+
+/**
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
+ */
+
+class Nexista_Singleton
+{
+
+    /**
+     * Hold an array of class instances
+     *
+     * @var object
+     */
+
+    private static $_instance_array = array();
+
+    /**
+     * Returns a class singleton.
+     *
+     * @return object class singleton instance
+     */
+
+
+    public static function singleton($class_name)
+    {
+        if (!isset(self::$_instance_array[$class_name])) {
+            self::$_instance_array[$class_name] = new $class_name();
+        }
+        return self::$_instance_array[$class_name];
+
+    }
+}
+
+?>
