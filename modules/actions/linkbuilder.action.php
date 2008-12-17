b6ff36adb4a4e9e66d12275ab7ec681436c2d291
Albert Lash <albert.lash@docunext.com>
2008-12-17 21:38:00 +0000

    new file, will an action be enough?

---
+++ modules/actions/linkbuilder.action.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * -File        Linkbuilder.Action.php
+ * -Copyright   Nexista
+ * -Author      Joshua Savage
+ * -Author      Albert Lash
+ *
+ * PHP version 5
+ *
+ * @category  Nexista
+ * @package   Nexista
+ * @author    Albert Lash <albert.lash@gmail.com>
+ * @copyright 0000 Nexista
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL
+ * @link      http://www.nexista.org/
+ */
+ 
+
+class Nexista_LinkbuilderAction extends Nexista_Action
+{
+
+    /**
+     * Function parameter array
+     *
+     * @var array
+     * @access  private
+     */
+
+    var $params = array(
+        );
+
+
+    /**
+     * Applies filter
+     *
+     * @return  boolean success
+     * @access  private
+     */
+
+    function main()
+    {
+
+    }
+} //end class
+
+?>
