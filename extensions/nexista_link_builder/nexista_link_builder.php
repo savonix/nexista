77f307e6127fad4a812f123ad97c9d52efe6ade3
Albert Lash <albert.lash@docunext.com>
2008-12-17 21:32:40 +0000

    need a link builder

---
+++ extensions/nexista_link_builder/nexista_link_builder.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Extension Name: Nexista Link Builder
+Extension URI:
+Description: Nexista Link Builder
+Version: 0.1
+Copyright: Nexista
+Author: Albert Lash
+License: LGPL
+*/
+
+
+
+function nexista_linkBuilder()
+{
+
+
+}
+
