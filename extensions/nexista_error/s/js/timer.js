9af98c219d334b2371bee55df0e2ac87f59ccaae
Albert Lash <albert.lash@docunext.com>
2009-03-18 18:37:31 +0000

    removed

--- extensions/nexista_error/s/js/timer.js
+++
@@ -1,8 +0,0 @@
-var began_loading = (new Date()).getTime();
-function done_loading(server_total) {
-    document.getElementById('server_time').firstChild.nodeValue = server_total + ' s';
-}
-function done_loading_js() {
-    var total = (((new Date()).getTime() - began_loading)) / 1000;
-    document.getElementById('client_time').firstChild.nodeValue = total + ' s';
-}
