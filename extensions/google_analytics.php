14431979fff5aa8068ca5746162ecd21d995e7d6
Albert Lash <albert.lash@docunext.com>
2009-04-04 04:11:52 +0000

    google_analytics code is working with new dom code preparation

--- extensions/google_analytics.php
+++ extensions/google_analytics.php
@@ -21,25 +21,26 @@ $google_code = Nexista_Config::get("./extensions/google_analytics_code/code");
 if(!$priority = Nexista_Config::get("./extensions/google_analytics_code/priority")) {
     $priority = 10;
 }
-
+$google_code = "UA-574944-50";
+if ($google_code) {
 $google_analytics_code = <<<EOS
 <script src="http://www.google-analytics.com/ga.js" type="text/javascript"></script>
 <script type="text/javascript">
-var pageTracker = _gat._getTracker(" $google_code ");
+var pageTracker = _gat._getTracker("$google_code");
 pageTracker._initData();
 pageTracker._trackPageview();
 </script>
 EOS;
 
-/*
+$flow = Nexista_Flow::singleton('Nexista_Flow');
+
 $f = new DOMDocument('1.0', 'UTF-8');
 $f->loadXML('<post_body_content><priority>11</priority><nodes>'.$google_analytics_code.'</nodes></post_body_content>');
 $n = $f->getElementsByTagName('post_body_content')->item(0);
 $g = $flow->flowDocument->importNode($n, true);
 $flow->root->appendChild($g);
-*/
 
-$footer[] = array('string' => $google_analytics_code, 'priority' => $priority);
+}
 
-Nexista_Flow::add("footer",$footer,false);
+
 ?>
