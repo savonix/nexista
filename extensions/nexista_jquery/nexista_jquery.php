e009dc5eb9f649c77ef03af41aed1b4948f0ca6d
Albert Lash <albert.lash@docunext.com>
2009-04-08 10:13:56 +0000

    I think that this is going to work out quite nicely

--- extensions/nexista_jquery/nexista_jquery.php
+++ extensions/nexista_jquery/nexista_jquery.php
@@ -11,4 +11,20 @@ License: LGPL
 --> */
 
 
+
+
+$flow = Nexista_Flow::singleton('Nexista_Flow');
+
+$mylink = $_SERVER['SCRIPT_NAME'];
+if ($_GET['nxrw_path']) {
+    $mylink = $_GET['nxrw_path'];
+}
+$my_script = '<script type="text/javascript" src="'.$mylink.'?nid=x-jquery.js">&#160;</script>';
+
+$f = new DOMDocument('1.0', 'UTF-8');
+$f->loadXML('<head_nodes><priority>100</priority><nodes>'.$my_script.'</nodes></head_nodes>');
+$n = $f->getElementsByTagName('head_nodes')->item(0);
+$g = $flow->flowDocument->importNode($n, true);
+$flow->root->appendChild($g);
+
 ?>
