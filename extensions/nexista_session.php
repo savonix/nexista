f80dc5d727441fd5fb947f0966c97b7b1f141ace
Albert Lash <albert.lash@docunext.com>
2009-05-22 17:54:32 +0000

    working on configurable sessions extension

--- extensions/nexista_session.php
+++ extensions/nexista_session.php
@@ -14,7 +14,7 @@
  *
  * @category Nexista_Extensions
  * @package  Nexista_Session
- * @author   Albert Lash <albert.lash@gmail.com>
+ * @author   Albert Lash <albert.lash@savonix.com>
  * @license  http://www.gnu.org LGPL
  * @version  SVN: 123
  * @link     http://www.nexista.org/
@@ -27,14 +27,17 @@ Configuration:
 <nexista_session>
     <placement>prepend</placement>
     <source>&includepath;extensions/nexista_session.php</source>
+    <handler>files</handler>
 </nexista_session>
 */
+
+$handler  = Nexista_Config::get('./extensions/nexista_session/handler');
 
 if(1==2) {
     ini_set('session.save_handler','user');
     include('HTTP/Session2.php');
     Nexista_Session::registerSessionStartHandler(array('HTTP_Session2','start'));
-} elseif (class_exists('Memcacxhe')) {
+} elseif (class_exists('Memcacxhe') && $handler=='memcached') {
     ini_set('session.save_handler','memcache');
     ini_set('session.save_path', 'tcp://192.168.3.3:11211?persistent=1&weight=2,tcp://memcached1.private.savonix.com:11211?persistent=1&weight=2&timeout=2&retry_interval=4,tcp://memcached2.private.savonix.com:11211?persistent=1&weight=2&timeout=2&retry_interval=4');
 } else {
